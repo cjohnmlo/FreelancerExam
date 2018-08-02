c94b8d0070631c540bd34ed2e792d8b6a1dfbe4d
John Lo <john.lo@cbsnorthstar.com>
2018-08-02 12:22:32 +0800

    added Bond, Alamofire, Kingfisher

---
+++ FreelancerTest/Protocols/DisplayableMovieSummary.swift
@@ -0,0 +1,14 @@
+//
+//  DisplayableMovieSummary.swift
+//  FreelancerTest
+//
+//  Created by Christian John Lo on 02/08/2018.
+//  Copyright © 2018 Christian John Lo. All rights reserved.
+//
+
+import Foundation
+import Bond
+
+protocol DisplayableMovieSummary {
+    
+}
